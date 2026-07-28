38648154d6be78d40b22396c74c0078ada935a85
CarlosGonzaga11 <carlospzk11@gmail.com>
2026-07-28 18:31:35 -0300

    alterando browserrouter para hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import { TrainProvider } from "./context/TrainContext";
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/hevolve">
+    <HashRouter>
     <AuthProvider>
       <TrainProvider>
         <App />
       </TrainProvider>
     </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
